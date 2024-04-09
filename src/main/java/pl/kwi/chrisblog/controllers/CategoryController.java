d959258d84665933b171e358e2dce316d4448772
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-04-09 16:55:46 +0200

    Update CategoryController

--- src/main/java/pl/kwi/chrisblog/controllers/CategoryController.java
+++ src/main/java/pl/kwi/chrisblog/controllers/CategoryController.java
@@ -1,27 +1,25 @@
 package pl.kwi.chrisblog.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 import pl.kwi.chrisblog.dtos.CategoryResponse;
 import pl.kwi.chrisblog.services.CategoryService;
 
-@RestController
-@CrossOrigin("${fe.url}")
+@Path("v1/category")
 public class CategoryController {
 
-
     private CategoryService categoryService;
-
     
-    @Autowired
+    @Inject
     public CategoryController(CategoryService categoryService) {
         this.categoryService = categoryService;
     }
 
-    @GetMapping("api/v1/category")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
     public CategoryResponse findCategories() {
         return categoryService.findCategories();
     }
