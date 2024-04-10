aa37171e16b8a61dbb1f2ad8fb0845ae90779a4e
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-04-10 08:35:31 +0200

    Improvement of TagService

--- src/main/java/pl/kwi/chrisblog/db/repositories/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package pl.kwi.chrisblog.db.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import pl.kwi.chrisblog.db.entities.CategoryEntity;
-
-public interface CategoryRepository extends CrudRepository<CategoryEntity, Long> {
-	
-}
--- src/main/java/pl/kwi/chrisblog/services/TagService.java
+++ src/main/java/pl/kwi/chrisblog/services/TagService.java
@@ -4,26 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import pl.kwi.chrisblog.db.entities.TagEntity;
-import pl.kwi.chrisblog.db.repositories.TagRepository;
 import pl.kwi.chrisblog.dtos.TagRequest;
 import pl.kwi.chrisblog.dtos.TagResponse;
 
-@Service
+@Stateless
 public class TagService {
 
-
-	private TagRepository tagRepository;
-
-    
-    @Autowired
-    public TagService(TagRepository tagRepository) {
-        this.tagRepository = tagRepository;
-    }
-
+	@PersistenceContext
+    private EntityManager em;
 
     public TagResponse findTags(TagRequest request) {
         
@@ -41,34 +34,47 @@ public class TagService {
 
     }
 
-    private List<TagEntity> getTagsByTag(TagRequest request) {		
-		return tagRepository.findAllByCategoryId(request.categoryId());		
+    private List<TagEntity> getTagsByTag(TagRequest request) {
+		
+		return em
+            .createQuery("SELECT t FROM TagEntity t JOIN t.articles a WHERE a.category.id = :categoryId GROUP BY t.name ORDER BY t.name ASC", TagEntity.class)
+			.setParameter("categoryId", request.getCategoryId())
+            .getResultList();
+
 	}
 
     private List<TagEntity> getTagsBySearch(TagRequest request) {
 
 		List<TagEntity> tags = new ArrayList<>();
 		
-		if (request.categoryId() != 0) {
-			tags = tagRepository.findAllByCategoryId(request.categoryId());
+		if (request.getCategoryId() != 0) {
+			tags = em
+            		.createQuery("SELECT t FROM TagEntity t JOIN t.articles a WHERE a.category.id = :categoryId GROUP BY t.name ORDER BY t.name ASC", TagEntity.class)
+					.setParameter("categoryId", request.getCategoryId())
+            		.getResultList();
 		} 
 
 		return tags;
 		
 	}
 
-    private List<TagEntity> getTagsByOtherCategory(TagRequest request) {		
-		return tagRepository.findAllByCategoryId(request.categoryId());		
+    private List<TagEntity> getTagsByOtherCategory(TagRequest request) {	
+
+		return em
+            .createQuery("SELECT t FROM TagEntity t JOIN t.articles a WHERE a.category.id = :categoryId GROUP BY t.name ORDER BY t.name ASC", TagEntity.class)
+			.setParameter("categoryId", request.getCategoryId())
+            .getResultList();		
+
 	}
 
     // ***** HELP METHODS ***** //
 
     private boolean isTag(TagRequest request) {		
-		return (request.tagId() != null);		
+		return (request.getTagId() != null);		
 	}
 
     private boolean isSearch(TagRequest request) {		
-		return (StringUtils.isNotBlank(request.searchText()));		
+		return (StringUtils.isNotBlank(request.getSearchText()));		
 	}
     
 }
