c8823f1eb08634e41a0dbaa00bf61316a3a5b820
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-04-11 18:41:37 +0200

    Update application URL

--- src/main/java/pl/kwi/chrisblog/ChrisBlogApplication.java
+++ src/main/java/pl/kwi/chrisblog/ChrisBlogApplication.java
@@ -3,7 +3,7 @@ package pl.kwi.chrisblog;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
-@ApplicationPath("api")
+@ApplicationPath("/")
 public class ChrisBlogApplication extends Application {
 
 }
